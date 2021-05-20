0f05d5859bf903aeb8f6ffe64173dab1a6323aed
donghwi <pari0130@gmail.com>
2021-05-20 16:23:00 +0900

    코틀린 - 흐름제어와 논리연산자

---
+++ src/06_흐름제어와논리연산자.kt
@@ -0,0 +1,39 @@
+import kotlin.math.PI
+import kotlin.math.abs
+
+// https://youtu.be/ZMOsbFCEeCY?list=PLQdnHjXZyYadiw5aV3p6DwUdXV2bZuhlN
+
+fun main() {
+    for (i in 1..10) {
+        if (i == 3) continue
+        println(i)
+    }
+
+    // 고전형태에서는 조건을 체크 후 break 를 수행
+    for (i in 1..10) {
+        for (j in 1..10) {
+            if (i == 1 && j == 2) {
+                println("test")
+                break
+            }
+        }
+    }
+
+    // 코틀린에서는 label 을 추가하여 사용
+    loop@ for (i in 1..10) {
+        for (j in 1..10) {
+            if (i == 1 && j == 2) break@loop
+            println("i : $i, j : $j")
+        }
+    }
+
+    println(true && false)
+    println(true || false)
+    println(!true)
+    println(!false)
+
+    var a = 6
+    var b = 7
+
+    println(a > 5 && b > 5)
+}
