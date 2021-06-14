27269cda02c2aec8ff5d88fee0f33df1476cc9a9
조동휘 <pari0130@gmail.com>
2021-06-14 09:35:53 +0900

    코틀린 - 비트연산 및 코루틴

---
+++ src/STEP27/비트연산_01.kt
@@ -0,0 +1,7 @@
+package STEP27
+
+// https://youtu.be/ZMOsbFCEeCY?list=PLQdnHjXZyYadiw5aV3p6DwUdXV2bZuhlN
+
+fun main() {
+
+}
---
+++ src/STEP28/코루틴을통한비동기처리_01.kt
@@ -0,0 +1,10 @@
+package STEP28
+
+// https://youtu.be/ZMOsbFCEeCY?list=PLQdnHjXZyYadiw5aV3p6DwUdXV2bZuhlN
+
+fun main() {
+    // Dispatchers.Default = 기본적인 백그라운드 동작
+    // Dispatchers.IO = i/o 에 최적화된 동작
+    // Dispatchers.Main = 메인 스레드에서 동작
+    // async {} await{}
+}
